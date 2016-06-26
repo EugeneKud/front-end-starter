1e826d1c4d922cf637a0eb33d0dbd817e4b30c36
Eugene Kudryavtsev <kudre1@bfh.ch>
2016-06-26 15:53:31 +0200

    Add html, clean and default tasks to gulpfile.babel.js and test htmlMin on src/index.html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,19 @@
 'use strict';
+
+import gulp from 'gulp';
+import del from 'del';
+import htmlMin from 'gulp-htmlmin';
+
+gulp.task('html', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(htmlMin({
+            collapseWhitespace: true, // most important, remove unnecessary spaces and line breaks
+            removeRedundantAttributes: true,
+            removeEmptyAttributes: true
+        }))
+        .pipe(gulp.dest('dist')); // output files
+});
+
+gulp.task('clean', () => del(['dist']));
+
+gulp.task('default', ['clean', 'html']);
