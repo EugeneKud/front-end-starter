cb8127ddcf19d2e88b35b309abcfce1367d703b5
Eugene Kudryavtsev <kudre1@bfh.ch>
2016-06-26 21:17:59 +0200

    Refactor gulpfile.babel.js by adding directories and paths in constants, by improving comments and by making clean task asynchronous

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,16 +4,26 @@ import gulp from 'gulp';
 import del from 'del';
 import htmlMin from 'gulp-htmlmin';
 
+const dir = {
+    src: 'src',
+    dest: 'dist'
+};
+
+const path = {
+    htmlSrc: `${dir.src}/**/*.html`,
+    htmlDest: `${dir.dest}`
+};
+
 gulp.task('html', () => {
-    return gulp.src('src/**/*.html')
+    return gulp.src(path.htmlSrc)
         .pipe(htmlMin({
             collapseWhitespace: true, // most important, remove unnecessary spaces and line breaks
             removeRedundantAttributes: true,
-            removeEmptyAttributes: true
+            removeEmptyAttributes: true // <html lang=""> gets removed
         }))
-        .pipe(gulp.dest('dist')); // output files
+        .pipe(gulp.dest(path.htmlDest)); // output files
 });
 
-gulp.task('clean', () => del(['dist']));
+gulp.task('clean', () => del.sync(['dist']));
 
 gulp.task('default', ['clean', 'html']);
