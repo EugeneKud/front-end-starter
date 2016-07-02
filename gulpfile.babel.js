33d496ee80bf26893e0968d9080c83fce05407b0
Eugene Kudryavtsev <kudre1@bfh.ch>
2016-07-02 17:32:13 +0200

    Refactor project folders and add support for plain JavaScript files to be compiled to main.js alongside TypeScript files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,26 +15,33 @@ const BROWSER_SYNC_RELOAD = browserSync.reload;
 
 const PROJECT = {
     SRC: 'src',
-    DEST: 'dist'
+    DEST: 'dist',
+    CSS_OUTPUT_FILE: 'main.css',
+    JS_OUTPUT_FILE: 'main.js'
 };
 
 const PATH = {
     HTML_SRC: `${PROJECT.SRC}/**/*.html`,
     HTML_DEST: `${PROJECT.DEST}`,
-    SCSS_SRC: `${PROJECT.SRC}/scss/**/*.scss`,
-    SCSS_DEST: `${PROJECT.DEST}/css`,
-    TS_SRC: `${PROJECT.SRC}/ts/**/*.ts`,
-    TS_DEST: `${PROJECT.DEST}/js`,
     NUNJUCKS_SRC: `${PROJECT.SRC}/**/*.nunjucks`,
-    NUNJUCKS_DEST: `${PROJECT.DEST}`,
-    NUNJUCKS_TEMPLATES: `${PROJECT.SRC}/nunjucks-templates`
+    NUNJUCKS_TEMPLATES_FOLDER: `${PROJECT.SRC}/nunjucks-templates`,
+    CSS_SRC: `${PROJECT.SRC}/styles/**/*.css`,
+    SCSS_SRC: `${PROJECT.SRC}/styles/**/*.scss`,
+    STYLES_DEST: `${PROJECT.DEST}/css`,
+    JS_SRC: `${PROJECT.SRC}/scripts/**/*.js`,
+    TS_SRC: `${PROJECT.SRC}/scripts/**/*.ts`,
+    SCRIPTS_DEST: `${PROJECT.DEST}/js`
+};
+
+const DEPENDENCIES = {
+    NORMALIZE_CSS: 'node_modules/normalize.css/normalize.css'
 };
 
 gulp.task('html', () => {
     return merge2(
-        gulp.src([PATH.NUNJUCKS_SRC, `!${PATH.NUNJUCKS_TEMPLATES}/**/*.nunjucks`])
+        gulp.src([PATH.NUNJUCKS_SRC, `!${PATH.NUNJUCKS_TEMPLATES_FOLDER}/**/*.nunjucks`])
             .pipe(gulpNunjucksRender({
-                path: [PATH.NUNJUCKS_TEMPLATES]
+                path: [PATH.NUNJUCKS_TEMPLATES_FOLDER]
             })),
         gulp.src(PATH.HTML_SRC) // regular HTML files
     )
@@ -49,26 +56,29 @@ gulp.task('html', () => {
 
 gulp.task('styles', () => {
     return merge2(
+        gulp.src(DEPENDENCIES.NORMALIZE_CSS),
         gulp.src(PATH.SCSS_SRC)
             .pipe(gulpSass()).on('error', gulpSass.logError),
-        gulp.src('node_modules/normalize.css/normalize.css')
+        gulp.src(PATH.CSS_SRC)
     )
-        .pipe(gulpConcat('main.css'))
+        .pipe(gulpConcat(PROJECT.CSS_OUTPUT_FILE))
         .pipe(gulpCssNano({
             discardComments: {
                 removeAll: true
             }
         }))
-        .pipe(gulp.dest(PATH.SCSS_DEST));
+        .pipe(gulp.dest(PATH.STYLES_DEST));
 });
 
-gulp.task('typescript', () => {
-    return gulp.src(PATH.TS_SRC)
-        .pipe(gulpTypescript({
-            noImplicitAny: true,
-            out: 'main.js'
-        }))
-        .pipe(gulp.dest(PATH.TS_DEST))
+gulp.task('scripts', () => {
+    return merge2(gulp.src(PATH.TS_SRC)
+            .pipe(gulpTypescript({
+                noImplicitAny: true
+            })),
+        gulp.src(PATH.JS_SRC)
+    )
+        .pipe(gulpConcat(PROJECT.JS_OUTPUT_FILE))
+        .pipe(gulp.dest(PATH.SCRIPTS_DEST))
 });
 
 gulp.task('serve', ['build'], () => {
@@ -77,15 +87,15 @@ gulp.task('serve', ['build'], () => {
         port: 3000
     });
     gulp.watch([PATH.HTML_SRC, PATH.NUNJUCKS_SRC], ['html', BROWSER_SYNC_RELOAD]);
-    gulp.watch(PATH.SCSS_SRC, ['styles', BROWSER_SYNC_RELOAD]);
-    gulp.watch(PATH.TS_SRC, ['typescript', BROWSER_SYNC_RELOAD]);
+    gulp.watch([PATH.CSS_SRC, PATH.SCSS_SRC], ['styles', BROWSER_SYNC_RELOAD]);
+    gulp.watch([PATH.JS_SRC, PATH.TS_SRC], ['scripts', BROWSER_SYNC_RELOAD]);
 });
 
 gulp.task('clean', () =>
     del.sync([PROJECT.DEST])
 );
 
-gulp.task('build', ['clean', 'html', 'styles', 'typescript']);
+gulp.task('build', ['clean', 'html', 'styles', 'scripts']);
 
 gulp.task('default',
     ['build']
